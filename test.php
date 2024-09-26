08c3a89527851d288d69c2dca7a085321fea321e
guhuo88889 <505894305@qq.com>
2024-09-26 09:34:10 +0800

    测试命令提交代码文件

---
+++ test.php
@@ -0,0 +1,46 @@
+<?php
+
+
+/**
+ * 1.注册github账户->创建一个仓库(new repository)，比如叫video_upload
+ * 2.克隆仓库video_upload到本地电脑上，需要按顺序安装两个软件，先装git和后装tortoiseGit(软件在360中有)
+ * 		安装git->一直点next->安装成功-》在桌面上右击查看是否有git
+ * 		安装tortoiseGit->一直点next->安装成功-》在桌面上右击查看是否有tortoiseGit
+ * 		a.克隆网上项目到本地----到指定位置-》右击git clone->选择github中的url-》ok----这个时候本地的代码中就可以添加自己的代码了
+ * 		b.提交代码----在你的本地项目位置-》右击tortoiseGit-》add/commit/push
+ * 
+ * 
+ */
+
+
+/**
+ * 加速器下载：https://steampp.net/，这个是用于访问github加速使用
+ * steam2.69:win7
+ * steam3.0:win10及以上
+ * 慎用
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
